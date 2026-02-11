4d32fffb547ca32222863c2da9f9ccd55d06e2df
Kevin Hernandez <kevinalbertohernandezgarcia@gmail.com>
2026-02-11 15:34:16 -0600

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Kevin Hernandez on 2/11/26.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
